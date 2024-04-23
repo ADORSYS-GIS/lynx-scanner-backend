184590d8f480420874db5254b271caeab081a7a9
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2024-04-23 10:30:42 +0100

    feat: project init

---
+++ src/main/java/com/ssegning/lynx/lynxbackend/LynxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssegning.lynx.lynxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LynxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LynxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssegning/lynx/lynxbackend/LynxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssegning.lynx.lynxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LynxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
